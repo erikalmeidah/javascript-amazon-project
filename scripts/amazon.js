781c1407cc499a1edef1ce25808e4f4b27c50229
Erik <e.dealmeida@ufl.edu>
2024-11-19 16:54:20 -0500

    use products.js files

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,6 @@
-// Hardcoded products
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
-
 // Generate products html
 let productsHTML = '';
+
 products.forEach((product) => {
     productsHTML += `
         <div class="product-container">
@@ -79,5 +53,5 @@ products.forEach((product) => {
         </div>`;
 });
 
-// put generated html into page
+// Put generated html into page
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
