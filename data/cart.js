faa9cdcffb4f82cdef68ee56cbed6893e72443cf
Erik <e.dealmeida@ufl.edu>
2024-11-21 14:55:57 -0500

    delete prod from cart functionality

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
 import { products } from "./products.js";
 
-export const cart = [{
+export let cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
 }, {
@@ -25,4 +25,14 @@ export function addToCart(productId) {
         quantity: 1
         });
     }
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+    cart.forEach((product) => {
+        if(product.productId !== productId){
+            newCart.push(product);
+        }
+    });
+    cart = newCart;
 }
