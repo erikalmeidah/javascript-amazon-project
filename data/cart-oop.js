25ec251d8e7e0bcdabf2b8181ed10c0c04c54dd3
Erik Companhone <e.comp2712@gmail.com>
2024-12-10 13:42:55 -0500

    create oop version of cart

---
+++ data/cart-oop.js
@@ -0,0 +1,66 @@
+import { products } from "./products.js";
+import {formatCurrency} from "../scripts/utils/money.js";
+import { getDeliveryOption } from "./deliveryOptions.js";
+
+function Cart(){
+    const cart = {
+        cartItems: undefined,
+    
+        loadFromStorage(){
+            this.cartItems = JSON.parse(localStorage.getItem('cart-oop')) || [];
+        },
+    
+        saveToStorage() {
+            localStorage.setItem('cart-oop', JSON.stringify(this.cartItems));
+        },
+    
+        addToCart(productId, quantity=1) {
+            let matchingItem;
+        
+            this.cartItems.forEach((cartItem) => {
+                if (cartItem.productId === productId) {
+                    matchingItem = cartItem;
+                }
+            });
+        
+            if (matchingItem) {
+                matchingItem.quantity += quantity;
+            } else {
+                this.cartItems.push({
+                productId,
+                quantity: quantity,
+                deliveryOptionId: "1"
+                });
+            }
+            this.saveToStorage();
+        },
+    
+        removeFromCart(productId) {
+            const newCart = [];
+            this.cartItems.forEach((product) => {
+                if(product.productId !== productId){
+                    newCart.push(product);
+                }
+            });
+            this.cartItems = newCart;
+            this.saveToStorage();
+        },
+    
+        updateDeliveryOption(productId, deliveryOptionId) {
+            let matchingItem;
+        
+            this.cartItems.forEach((cartItem) => {
+                if (cartItem.productId === productId) {
+                    matchingItem = cartItem;
+                }
+            });
+        
+            matchingItem.deliveryOptionId = deliveryOptionId;
+            this.saveToStorage();
+        }
+    };
+    return cart;
+}
+
+const cart = Cart();
+cart.loadFromStorage();
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/PaymentSummary.js";
 import { addToOrder } from "../data/orderList.js";
 import { cart } from "../data/cart.js";
-
+import '../data/cart-oop.js';
 //render order summary to screen
 renderOrderSummary();
 renderPaymentSummary();
